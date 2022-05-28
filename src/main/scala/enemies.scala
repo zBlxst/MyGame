dd3d562174357a04ce91b75fbe2fbb9c2709f29c
zBlxst <tomloulou@yahoo.fr>
2022-05-28 22:39:06 +0200

    added intentions

--- src/main/scala/enemies.scala
+++ src/main/scala/enemies.scala
@@ -7,6 +7,8 @@ object Enemy1 extends Enemy ("ennemy.png") {
 
     hpMax = 100
     hp = 100
+
+    allActions = List("Attack 10", "Attack 5", "Shield 5")
 }
 
 object Enemy2 extends Enemy ("ennemy.png") {
@@ -18,4 +20,6 @@ object Enemy2 extends Enemy ("ennemy.png") {
 
     hpMax = 20
     hp = 20
+
+    allActions = List("Attack 5", "Attack 3", "Shield 5")
 }
--- src/main/scala/enemy.scala
+++ src/main/scala/enemy.scala
@@ -1,10 +1,50 @@
 import java.util.concurrent.TimeUnit
+import java.awt.Graphics
+import java.awt.Color
 class Enemy (imgName_ : String) extends Character (imgName_) {
+
+    var nextAction : String = "Attack 0"
+    var allActions : List[String] = List("Attack 0")
+
+    override def initialise : Unit = {
+        super.initialise
+        chooseNextAction
+    }
+
+
     override def newTurn : Unit = {
         super.newTurn
-        dealDamage(10, Player)
+        doAction
+        chooseNextAction
         TimeUnit.MILLISECONDS.sleep(2000)
     }
+
+    def doAction : Unit = {
+        if (nextAction.startsWith("Attack")) {
+            var splited = nextAction.split(" ")
+            dealDamage(splited(1).toInt, Player)
+        } else if (nextAction.startsWith("Shield")) {
+            var splited = nextAction.split(" ")
+            gainShield(splited(1).toInt)
+        }
+    }
+
+    def chooseNextAction : Unit = {
+        nextAction = allActions(scala.util.Random.nextInt(allActions.length))
+    }
+
+    override def display (g : Graphics) : Unit = {
+        super.display(g)
+        g.setColor(Color.BLACK)
+        
+        var metrics = g.getFontMetrics
+        var text = "Next action : " + nextAction
+        var strX = posX + sizeX/2 - metrics.stringWidth(text)/2
+        var strY = posY - 50 - (if (shield > 0) 20 else 0) - metrics.getHeight/2 + metrics.getAscent
+        g.drawString(text, strX, strY)
+    }
+
+    
 
     override def die : Unit = {
         super.die
